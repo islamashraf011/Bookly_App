469436200b1b7b0c6a5949cae90d032f0d46602b
I S L A M <islamashraf1539@gmail.com>
2024-07-14 01:29:53 +0300

    Create Horizontal Books List View Item

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/Core/utils/responsive_size.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
+import 'horizontal_books_listview_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -15,6 +16,7 @@ class HomeViewBody extends StatelessWidget {
       child: const Column(
         children: [
           CustomAppBar(),
+          HorizontalBooksListViewItem(),
         ],
       ),
     );
