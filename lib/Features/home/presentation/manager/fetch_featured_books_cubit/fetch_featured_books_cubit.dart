1f76b50405de7232c55315cd39df721047a7c740
I S L A M <islamashraf1539@gmail.com>
2024-08-04 05:25:55 +0300

    Create Fetch Featured Books Cubit Method

--- lib/Features/home/presentation/manager/fetch_featured_books_cubit/fetch_featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/fetch_featured_books_cubit/fetch_featured_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../../data/models/book_model/book_model.dart';
@@ -6,5 +7,26 @@ import '../../../data/models/book_model/book_model.dart';
 part 'fetch_featured_books_state.dart';
 
 class FetchFeaturedBooksCubit extends Cubit<FetchFeaturedBooksState> {
-  FetchFeaturedBooksCubit() : super(FetchFeaturedBooksInitial());
+  FetchFeaturedBooksCubit(this.homeRepo) : super(FetchFeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FetchFeaturedBooksLoadingState());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+// Used Fold from Dartz Package to Handel (left / right) return
+    result.fold(
+      (failure) {
+        emit(
+          FetchFeaturedBooksFailureState(failure.errMessage),
+        );
+      },
+      (books) {
+        emit(
+          FetchFeaturedBooksSuccessState(books),
+        );
+      },
+    );
+  }
 }
